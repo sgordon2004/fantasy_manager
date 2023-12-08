ab73af2acd5dd53e7742d9c2c854bac68b544e00
sgordon2004 <syrrgordon@Syrrs-MBP-2.fios-router.home>
2023-12-08 13:26:08 -0500

    first commit

---
+++ stats.py
@@ -0,0 +1,14 @@
+from nba_api.stats.endpoints import playercareerstats
+
+# Nikola Jokić
+career = playercareerstats.PlayerCareerStats(player_id='203999') 
+
+# pandas data frames (optional: pip install pandas)
+career.get_data_frames()[0]
+
+# json
+career.get_json()
+
+# dictionary
+str = career.get_dict()
+print(str)
